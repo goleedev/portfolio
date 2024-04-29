d8f3e078eaa65a2c41dd29cfac9f0050babfd6d9
goleedev <golee.dev@gmail.com>
2024-04-29 13:43:54 +0900

    refactor update not found page

--- app/blog/[slug]/not-found.tsx
+++ app/blog/[slug]/not-found.tsx
@@ -1,23 +1,18 @@
-'use client';
-
 import { FrownIcon } from 'lucide-react';
-import { useRouter } from 'next/navigation';
+import Link from 'next/link';
 
 export default function NotFound() {
-  const router = useRouter();
-
   return (
-    <section className="w-full flex flex-col items-center justify-center text-center max-w-screen-md min-h-[calc(100vh-72px)]">
+    <section className="w-full flex flex-col items-center justify-center text-center max-w-screen-md min-h-[calc(100vh-200px)]">
       <FrownIcon size={40} className="text-gray-400" />
       <h2 className="text-xl font-semibold">404 Not Found</h2>
       <p>Could not find the blog post.</p>
-      <button
-        type="button"
-        onClick={() => router.back()}
+      <Link
+        href={'/blog'}
         className="mt-4 rounded-md bg-blue-500 px-4 py-2 text-sm text-white transition-colors bg-orange hover:bg-orange-light dark:bg-blue dark:hover:bg-blue-light"
       >
         Go Back
-      </button>
+      </Link>
     </section>
   );
 }
