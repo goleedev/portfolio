bf3a31a38f32e6b20bc30f3482b0cefb45b10018
goleedev <golee.dev@gmail.com>
2024-05-20 00:16:22 +0900

    refactor update open graph image

--- app/layout.tsx
+++ app/layout.tsx
@@ -20,6 +20,12 @@ export const metadata: Metadata = {
     url: 'https://golee.me',
     siteName: 'GO Lee',
     locale: 'en_GB',
+    images: [
+      {
+        url: '/opengraph-image.png',
+        alt: 'GO Lee Blog Open Graph Image',
+      },
+    ],
   },
 };
 
